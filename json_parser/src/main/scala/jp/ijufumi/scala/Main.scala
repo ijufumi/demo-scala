06222bad86c5cbe07245dd4be1f37c15b8e2d9e5
ijufumi <ijufumi@gmail.com>
2019-07-11 19:16:02 +0900

    Modify process to using match expression.

--- json_parser/src/main/scala/jp/ijufumi/scala/Main.scala
+++ json_parser/src/main/scala/jp/ijufumi/scala/Main.scala
@@ -4,6 +4,16 @@ import scala.io.Source
 
 object Main extends App {
   val sources = Source.fromFile("src/main/resources/sample.json")
-  sources.foreach(println)
+
+  sources.foreach(s => {
+    s.toString match {
+      case "{" | "}" => println
+      case "[" | "]" => println
+      case "\"" => println
+      case ":" => println
+      case "," => println
+      case x : String => print(x.trim) // print key/value
+    }
+  })
   sources.close()
 }
