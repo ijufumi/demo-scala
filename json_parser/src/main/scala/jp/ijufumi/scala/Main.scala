49579f78036f793a0156a8474b8b219c9f650d33
ijufumi <ijufumi@gmail.com>
2019-07-11 16:35:01 +0900

    Add sample.json

--- json_parser/src/main/scala/jp/ijufumi/scala/Main.scala
+++ json_parser/src/main/scala/jp/ijufumi/scala/Main.scala
@@ -1,5 +1,9 @@
 package jp.ijufumi.scala
 
+import scala.io.Source
+
 object Main extends App {
-  println("Hello, World!")
+  val sources = Source.fromFile("src/main/resources/sample.json")
+  sources.foreach(x => println(x))
+  sources.close()
 }
