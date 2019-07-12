2124c06e6eeb4f6841c58feeb914ab62dd64101c
ijufumi <ijufumi@gmail.com>
2019-07-12 17:25:09 +0900

    Modify check process.
    Change from equals to regex.

--- json_parser/src/main/scala/jp/ijufumi/scala/Main.scala
+++ json_parser/src/main/scala/jp/ijufumi/scala/Main.scala
@@ -5,13 +5,21 @@ import scala.io.Source
 object Main extends App {
   val sources = Source.fromFile("src/main/resources/sample.json")
 
+  val checkComma = "(,)".r
+  val checkCologne = "(:)".r
+  val checkQuotation = "(\")".r
+  val checkStartBracket1 = "(\\{)".r
+  val checkEndBracket1 = "(\\})".r
+  val checkStartBracket2 = "(\\[)".r
+  val checkEndBracket2 = "(\\])".r
+
   sources.foreach(s => {
     s.toString match {
-      case "{" | "}" => println
-      case "[" | "]" => println
-      case "\"" => println
-      case ":" => println
-      case "," => println
+      case checkStartBracket1(_) | checkStartBracket2(_) => print(_)
+      case checkEndBracket1(_) | checkEndBracket2                 (_)=> println
+      case checkQuotation(_) => print(_)
+      case checkCologne(_) => println
+      case checkComma(_) => println
       case x : String => print(x.trim) // print key/value
     }
   })
